0b513a57de3155e2f34a755226b72fa54e95f693
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-07-01 16:49:29 +0200

    random number generator

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 window.onload = function(){
     displayEntireBoard()
 }
-
-
 const displayEntireBoard= function(){
     let bingoBoardNode = document.getElementById("bingo-board-container")
     for (let cellNumber = 1; cellNumber <= 76; cellNumber++) {
@@ -11,5 +9,11 @@ const displayEntireBoard= function(){
     cellNode.innerText = cellNumber 
     cellNode.classList.add("board-cells") 
     bingoBoardNode.appendChild(cellNode)
+    cellNode.onclick = randomNumberGenerator(event)
+
 }
+}
+
+const randomNumberGenerator = function (event) {
+    Math.round(Math.random()* 75 + 1)
 }
