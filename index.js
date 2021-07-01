facb80eed0a42180b9345dd32a8ff410f7e6f51d
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-07-01 19:09:15 +0200

    generating random number and highlighted cell

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 
 window.onload = function(){
     displayEntireBoard()
+    randomNumberGenerator()
 }
 const displayEntireBoard= function(){
     let bingoBoardNode = document.getElementById("bingo-board-container")
     for (let cellNumber = 1; cellNumber <= 76; cellNumber++) {
-    let cellNode = document.createElement("div") 
-    cellNode.innerText = cellNumber 
-    cellNode.classList.add("board-cells") 
-    bingoBoardNode.appendChild(cellNode)
-    cellNode.onclick = randomNumberGenerator(event)
-
-}
+        let cellNode = document.createElement("div") 
+        cellNode.innerText = cellNumber 
+        cellNode.classList.add("board-cells") 
+        bingoBoardNode.appendChild(cellNode)
+    }
 }
 
-const randomNumberGenerator = function (event) {
-    Math.round(Math.random()* 75 + 1)
+const randomNumberGenerator = function () {
+    let random = Math.round(Math.random()* 75 + 1)
+    const genNumber = document.getElementById("generated-number");
+    genNumber.innerText = "Number: " + random
+    const cellSelected = document.querySelectorAll("#bingo-board-container .board-cells");
+        cellSelected[random - 1].classList.add("selected");
 }
